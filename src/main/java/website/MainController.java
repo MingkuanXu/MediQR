7aa72d310ed15d3a3bbc576b918af84338bedd96
Mingkuan Xu <mingkuan.xu@duke.edu>
2019-11-03 00:43:14 -0400

    Handle conflicts.

--- src/main/java/website/MainController.java
+++ src/main/java/website/MainController.java
@@ -5,7 +5,11 @@ package website;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +17,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.time.LocalDateTime;    
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;   
 
 
 @Controller
@@ -43,7 +48,7 @@ public class MainController {
 		description[2] = age;
 		description[3] = syndrome;
 		description[4] = diagnosis;
-		
+
 		String password = UserDao.findUserPassword(pid);
 		
 		Profile patient = new Profile(pid,description);
@@ -54,12 +59,50 @@ public class MainController {
 	
 		String re = "redirect:/image?url="/*+keyword;*/+URLEncoder.encode(imageUrl, "utf-8"); //进行搜索
 		return re;
-		
-//		return imageUrl;
-//		return "http://google.com";
+
 	}
 	
 	
+
+	@RequestMapping("/decode")
+
+	public String decode(Model model) throws IOException {
+
+        URL url = new URL("https://api.qrserver.com/v1/read-qr-code/?fileurl=https://github.com/MingkuanXu/MediQR/blob/master/src/main/resources/static/FamilyDinner2.jpeg");
+        URLConnection conn = url.openConnection();
+
+        // open the stream and put it into BufferedReader
+        BufferedReader br = new BufferedReader(
+                           new InputStreamReader(conn.getInputStream()));
+
+        String inputLine;
+        String jsonInfo = "";
+        while ((inputLine = br.readLine()) != null) {
+                jsonInfo+=inputLine;
+        }
+        
+        br.close();
+
+        System.out.println(jsonInfo);
+        
+        
+        int pid = 1;
+        String name = "Name1";
+        String age = "20";
+        String gender = "Male";
+        String syndrome = "Fever";
+        String diagonosis = "Malaria";
+   
+		model.addAttribute("pid",pid);
+		model.addAttribute("name",name);
+		model.addAttribute("age",age);
+		model.addAttribute("gender",gender);
+		model.addAttribute("syndrome",syndrome);
+		model.addAttribute("diagonosis",diagonosis);
+		
+		return "profile";
+		
+	}
 	
 	
 	@RequestMapping("/image")
@@ -68,54 +111,12 @@ public class MainController {
 		return "image";
 	}
 	
-	/*@PostMapping("/")
-	public String encode2(@RequestParam(name="pid") String pid,
-	 @RequestParam(name="description") String description) {
-		System.out.printf("Index Recieved");
-		return "/";
-	}*/
-	/*
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-    
-    
-    @GetMapping("/post")
-    public String displayPostPage(Model model) throws FileNotFoundException{
-    		List<Map<String, String>> comments = UserDao.extractFromFile();
-    		model.addAttribute("comments",comments);
-    		return "healthy_posts";
-    }
-    @PostMapping("/post")
-    public String comment(@RequestParam(name="name") String name,
-    						 @RequestParam(name="content") String content) throws IOException {    
-    	
-    		// Get local time and format it
-    		LocalDateTime now = LocalDateTime.now(); 
-    		String date = now.toString();
-    		date = date.split("\\.")[0];
-    		date = String.join(" ",date.split("T"));
-    		
-    		
-        UserDao.writeToFile(name,date,content);
-		//return "redirect:/";
-        return "redirect:/post";
-    }
-    */
-    
-    /*
-    @RequestMapping("/display")
-    public String homepage(Model model) {
-    		//List<Map<String, String>> comments = jdbcTemplate.queryForList("SELECT * FROM comments");
-    		//System.out.println(comments);
-        //model.addAttribute("comments",comments);
-    		return "test";
-    }*/
-    
-    //@RequestMapping("/")
 	
+	@RequestMapping("/")
+	public String mainpage() {
+		return "index";
+	}
+
 	
 	
 }
